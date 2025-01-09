c4b66712d16e234f0cec3577be668cf37d33c310
Yehia1425 <yehieamostafa10@gmail.com>
2025-01-09 17:00:58 +0200

    second-commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-portfolio',
@@ -8,4 +9,50 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  images: string[] = [
+    "/images/poert1.png",
+    "/images/port2.png",
+    "/images/port3.png",
+    "/images/poert1.png",
+    "/images/port2.png",
+    "/images/port3.png",
+  ]
+  indexx: number = 0;
+  srcimg: string = '';
+  flag: boolean = true;
+  showContainer(src: string, index: number): void {
+    this.srcimg = src;
+    this.flag = false;
+    this.indexx = index;
+  }
+
+  slide(step: number): void {
+    this.indexx = this.indexx + step;
+    if (this.indexx < 0) {
+      this.indexx = this.images.length - 1;
+    }
+    else if (this.indexx === this.images.length) {
+      this.indexx = 0;
+    }
+    this.srcimg = this.images[this.indexx];
+  }
+
+  closeSlie(eInfo: MouseEvent): void {
+    this.flag = true;
+  }
+
+  notCloseSlie(eInfo: MouseEvent): void {
+    eInfo.stopPropagation();
+  }
+
+
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    if (event.key === 'ArrowRight') {
+      this.slide(1);
+    } else if (event.key === 'ArrowLeft') {
+      this.slide(-1);
+    }
+  }
 }
